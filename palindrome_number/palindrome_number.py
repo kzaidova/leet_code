d29734beca708a81ceef8fc3d8fe64c0ab42ffa9
kzaidova <katerina00.00@bk.ru>
2023-11-04 17:52:54 +0400

    - add solution palindrome_number.py https://leetcode.com/problems/palindrome-number

---
+++ palindrome_number/palindrome_number.py
@@ -0,0 +1,37 @@
+
+class Solution:
+
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        tmp = []
+        while x>0:
+            b = x % 10
+            tmp.append(b)
+            x = x // 10
+        i = 0
+        j = len(tmp) - 1
+        while i < len(tmp) / 2:
+            if tmp[i] != tmp[j]:
+                return False
+            i = i + 1
+            j =  j - 1
+        return True
+a = Solution()
+a.isPalindrome(x = 12321)
+
+
+class Solution_2:
+
+    def isPalindrome(self, x: int) -> bool:
+        for i in range(0, len(str(x))):
+            a = str(x)
+            b = a[::-1]
+            if a == b:
+                return True
+            else:
+                return False
+
+a = Solution_2()
+
+a.isPalindrome(x = 12321)
